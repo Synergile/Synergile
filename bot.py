c5cbb151c9a513e431bed0dd0175c637a00235b0
SSwiftGamer440 <SSwiftGamer440@gmail.com>
2020-05-22 00:18:57 -0700

    updated 0 lines of code, this commit is empty /s

--- bot.py
+++ bot.py
@@ -15,30 +15,32 @@ async def profile(ctx, member= None):
     if member is None:
         mem = ctx.guild.get_member(ctx.author.id)
         embed = discord.Embed(title= ctx.author.name, color= 0x00ff00)
-        embed.add_field(name= 'Joined the server at:', value = mem.joined_at, inline=False)
-        embed.add_field(name= 'Is Bot:', value = mem.bot, inline=False)
-        embed.add_field(name= "Username+Discrim:", value = ctx.author, inline=False)
-        embed.add_field(name= "Highest role:", value = mem.top_role.name, inline=False)
-        embed.add_field(name= "ID:", value = mem.id, inline= False)
+        embed.add_field(name= 'Joined at', value = mem.joined_at, inline=False)
+        embed.add_field(name= 'Is Bot', value = mem.bot, inline=False)
+        embed.add_field(name= "Username+Discrim", value = ctx.author, inline=False)
+        embed.add_field(name= "Highest role", value = mem.top_role.name, inline=False)
+        embed.add_field(name= "ID", value = mem.id, inline= False)
         await ctx.send(embed=embed)
     else:
-         users = []
-       # for x in ctx.guild.members:
-       #     if member in x.name:
-       #         users.append(x.name)
-         users.append(await ctx.guild.fetch_member(x.id))       
+        users = []
+        for x in ctx.guild.members:
+            if member in x.name:
+                users.append(x.name)
+                
             if len(users) >= 2:
                 embed = discord.Embed(title = "Multiple Users Found", color = 0xff0000)
                 embed.add_field(name=f"Found {len(users)} users, try being more specific", value = '\n'.join(users), inline = False)
                 await ctx.send(embed=embed)
                 break
 
+        if "@" in member:
+            user = member
         mc = commands.MemberConverter()
-        mem = await mc.convert(ctx, users[0])
+        mem = await mc.convert(ctx, user)
         embed = discord.Embed(title= mem.name, color= 0x00ff00)
         embed.add_field(name= 'Joined at', value = mem.joined_at, inline=False)
         embed.add_field(name= 'Is Bot', value = mem.bot, inline=False)
-        embed.add_field(name= "Username+Discrim", value = mem.name+mem.tag, inline=False)
+        embed.add_field(name= "Username+Discrim", value = mem.name, inline=False)
         embed.add_field(name= "Highest role", value = mem.top_role.name, inline=False)
         embed.add_field(name= "ID", value = mem.id, inline= False)
         await ctx.send(embed=embed)
@@ -46,6 +48,7 @@ async def profile(ctx, member= None):
                 
 
 
-token = os.getenv('BOT_TOKEN')
-
-bot.run('NzEzMTI5ODUxMDU3MzQwNDg3.XsboTw.b2c_QAprAh2IONu38aY-t6Giz64')
+with open('config.config', 'r') as f:
+    tok = f.readline()
+    tok.replace('\n', "")
+bot.run(tok)
