9076298a040bf158cd9b01bca32952b7b396f90c
SSwiftGamer440 <SSwiftGamer440@gmail.com>
2020-05-22 21:51:20 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import datetime
 from discord.ext import commands
 from discord.ext import tasks
 import os
+import math
 desc= "Moderation bot engineered by CodeWritten, wakfi, and jedi3"
 bot = commands.Bot(command_prefix='!', case_insensitive=True, description=desc)
 @bot.event
@@ -15,18 +16,16 @@ async def profile(ctx, member= None):
     mc = commands.MemberConverter()
     if member is None:
         mem = ctx.guild.get_member(ctx.author.id)
-    elif '@' in member:
-        mem = await mc.convert(ctx, member)
-    embed = profileEmbed(mem)
-    await ctx.send(embed=embed)
+        embed = profileEmbed(mem)
+        await ctx.send(embed=embed)
 
-def profileEmbed(mem)
+def profileEmbed(mem):
     #avoiding magic numbers
     DISCORD_EPOCH = 1420070400000 #first second of 2015
-    userMilliseconds = mem.id/math.pow(2,22) + DISCORD_EPOCH
+    userMilliseconds = int(mem.id/math.pow(2,22) + DISCORD_EPOCH)
     embed = discord.Embed(title= mem.name, color= 0x00ff00)
     embed.add_field(name= 'Joined the server at:', value = mem.joined_at, inline=False)
-    embed.add_field(name= 'Joined Discord:', value = datetime.utcfromtimestamp(userMilliseconds//1000).replace(microsecond=userMilliseconds%1000*1000), inline=False)
+    embed.add_field(name= 'Joined Discord:', value = datetime.datetime.utcfromtimestamp(int(userMilliseconds//1000)).replace(microsecond=userMilliseconds%1000*1000), inline=False)
     embed.add_field(name= 'Is Bot:', value = mem.bot, inline=False)
     embed.add_field(name= "Username+Discrim:", value = f'{mem.name}#{mem.discriminator}', inline=False)
     embed.add_field(name= "Highest role:", value = mem.top_role.name, inline=False)
