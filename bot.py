a07668d3b3a524cee02114b113f4425eaf9257c3
Walker Gray <55608093+wakfi@users.noreply.github.com>
2020-05-22 21:17:50 -0700

    Revert "Add calculation of date user joined Discord"
    This reverts commit 41f600a65888efac74c907f5d52b705d5076dab5.

--- bot.py
+++ bot.py
@@ -21,12 +21,8 @@ async def profile(ctx, member= None):
     await ctx.send(embed=embed)
 
 def profileEmbed(mem)
-    #avoiding magic numbers
-    DISCORD_EPOCH = 1420070400000 #first second of 2015
-    ms = mem.id/math.pow(2,22) + DISCORD_EPOCH
     embed = discord.Embed(title= mem.name, color= 0x00ff00)
     embed.add_field(name= 'Joined the server at:', value = mem.joined_at, inline=False)
-    embed.add_field(name= 'Joined Discord:', value = datetime.utcfromtimestamp(ms//1000).replace(microsecond=ms%1000*1000), inline=False)
     embed.add_field(name= 'Is Bot:', value = mem.bot, inline=False)
     embed.add_field(name= "Username+Discrim:", value = f'{mem.name}#{mem.discriminator}', inline=False)
     embed.add_field(name= "Highest role:", value = mem.top_role.name, inline=False)
