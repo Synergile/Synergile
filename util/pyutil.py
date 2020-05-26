7421aabb4c47544ead03d2e575d66597db2e9482
wakfi <55608093+wakfi@users.noreply.github.com>
2020-05-25 22:37:46 -0700

    Add StringBuilder class for efficient string concatenation

--- util/pyutil.py
+++ util/pyutil.py
@@ -1,5 +1,6 @@
 import asyncio
 import re
+from .stringbuilder import StringBuilder
 OPTION_REGEX = re.compile(' -[a-zA-Z]+')
 FLAGS_REGEX = re.compile('-')
 SPACE_REGEX = re.compile(' ')
@@ -11,14 +12,15 @@ def rreplace(s, old, new, occurrence):
     return new.join(li)
 
 #efficiently build a string of variable length when multiple members are found following a query
-def buildMultiMatchString(command_prefix,command,mem,member):
-    strBuilder=f'Found {len(mem)} possible matches for "{member}":```'
-    strBuilder=''.join([strBuilder, ''.join(f'\n{index+1}. {memMatch}' for index,memMatch in enumerate(mem))])
-    strBuilder+=f'```'
+def buildMultiMatchString(command_prefix,command,mem,member): 
+    strBuilder = StringBuilder(f'Found {len(mem)} possible matches for "{member}":```')
+    for index,memMatch in enumerate(mem):
+        strBuilder.append(f'\n{index+1}. {memMatch}')
+    strBuilder.append(f'```')
     if len(mem)==5:
-        strBuilder+=f'\n(number of matches shown is capped at 5, there may or may not be more)'
-    strBuilder+=f'\nTry using the {command_prefix}{command} command again with a more specific search term!'
-    return strBuilder
+        strBuilder.append(f'\n(number of matches shown is capped at 5, there may or may not be more)')
+    strBuilder.append(f'\nTry using the {command_prefix}{command} command again with a more specific search term!')
+    return strBuilder.to_string()
 
 #split arguments into seperate (-x) flags where x can be any letter or sequence of letters
 #each flag will be seperated even if given as a single string and returned as a list
---
+++ util/stringbuilder.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+from io import StringIO
+
+'''
+MIT License
+
+Copyright (c) 2020 Walker Gray
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+class StringBuilder:
+    '''Provides efficient and performant concatenation of strings'''
+    
+    def __init__(self, initial_str=None):
+        '''Construct a new StringBuilder. An initial value may optionally be provided'''
+        self.file_str = StringIO()
+        if initial_str is not None:
+            self.file_str.write(str(initial_str))
+        
+    @classmethod
+    def from_list(cls, data: list):
+        '''
+        Returns a new StringBuilder using the provided list of initial values. Values are concatenated together in order.
+        Accepts any values that str() can be called on.
+        '''
+        self = cls.__new__(cls)
+        self.file_str = StringIO()
+        for str in data:
+            self.file_str.write(str(str))
+        return self
+    
+    def append(self, data):
+        '''Appends data to the StringBuilder. Accepts any value that str() can be called on. Returns self to enable successive chaining'''
+        self.file_str.write(str(data))
+        return self
+
+    def to_string(self)-> str:
+        '''Returns a string representation of the data'''
+        return self.file_str.getvalue()
+        
+    def len(self)-> int:
+        '''Returns the length of the string currently'''
+        return len(self.file_str.getvalue())
+        
+        
