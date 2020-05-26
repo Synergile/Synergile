b3eb527301aee25968e12e94678e689e59a3ef75
wakfi <55608093+wakfi@users.noreply.github.com>
2020-05-25 23:26:41 -0700

    Update syntax for commands in bot help

--- bot.py
+++ bot.py
@@ -229,11 +229,12 @@ async def play(ctx, url: str):
 @bot.command(pass_context=True)
 async def help(ctx):
     author = ctx.message.author
-    embed = discord.Embed(colour = discord.Colour.orange(), title = 'Help', timestamp = datetime.now(timezone.utc))
+    embed = discord.Embed(colour = discord.Colour.orange(), title = 'Help', timestamp = datetime.now(timezone.utc),
+        description='Syntax note: A "member resolvable" is a user mention, user ID, or a username fragment that can be resolved to a single user in the server (which can be an entire username)')
     embed.add_field(name=f'{bot.command_prefix}ping', value='Returns Pong!', inline=False)
-    embed.add_field(name=f'{bot.command_prefix}profile [@user | userID]', value='Display information about a given user', inline=False)
-    embed.add_field(name=f'{bot.command_prefix}kick <@user | userID>', value='Kicks a member from the server', inline=False)
-    embed.add_field(name=f'{bot.command_prefix}ban <@user | userID>', value='Bans a member from the server', inline=False)
+    embed.add_field(name=f'{bot.command_prefix}profile [member resolvable]', value='Display information about a given user', inline=False)
+    embed.add_field(name=f'{bot.command_prefix}kick <member resolvable>', value='Kicks a member from the server', inline=False)
+    embed.add_field(name=f'{bot.command_prefix}ban <member resolvable>', value='Bans a member from the server', inline=False)
     embed.set_footer(text= f"Requested by {author}", icon_url=author.avatar_url)    
     await ctx.send(embed=embed)
 
