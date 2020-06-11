0cbf0d58929cb6b836fd2465b4a087a5f3aa3698
wakfi <55608093+wakfi@users.noreply.github.com>
2020-06-11 14:59:35 -0700

    Add restart command
    Squash: Fix typo

---
+++ commands/restart.py
@@ -0,0 +1,20 @@
+import sys
+import discord
+from discord.ext import commands
+
+
+'''
+You must use a process manager for this command to work fully; I reccomend pm2 (surprisingly it works with python scripts)
+'''
+class Restart(commands.Cog, name='Restart'):
+	def __init__(self, bot):
+		self.bot = bot
+	
+	@commands.command(desc="Restart the bot")
+	async def restart(self,ctx):
+		await self.bot.change_presence(status=discord.Status.offline)
+		await ctx.send(f"Restarting...")
+		sys.exit(0)
+
+def setup(bot):
+	bot.add_cog(Restart(bot))
