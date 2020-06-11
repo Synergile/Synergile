98ea79a126c9a2e47ce33971a9e5d13069ee5117
wakfi <55608093+wakfi@users.noreply.github.com>
2020-06-11 15:48:38 -0700

    Update ping with client endpoint latency

--- commands/ping.py
+++ commands/ping.py
@@ -10,7 +10,11 @@ class Ping(commands.Cog, name='Ping'):
 	
 	@commands.command(desc="Displays connection speed info")
 	async def ping(self,ctx):
-		await ctx.send('Pong! {} ms'.format(self.bot.latency*1000))
+		try:
+			m = await ctx.send('Ping?')
+			await m.edit(content=f'🏓 Pong! Latency is {int((snowflake_time(m.id) - snowflake_time(ctx.message.id)).microseconds/1000)}ms. API Latency is {round(self.bot.latency*1000)}ms')
+		except Exception as e:
+			await ctx.send(repr(e))
 
 def setup(bot):
 	bot.add_cog(Ping(bot))
