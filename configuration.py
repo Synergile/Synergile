17559cbe009ae211ae1e7cbb44a69f3374a94515
DaRealCodeWritten <SSwiftGamer440@gmail.com>
2021-02-21 01:27:46 -0800

    Rework configuration.py

--- configuration.py
+++ configuration.py
@@ -1,35 +1,31 @@
 import os
 import ensure_path
+class MissingTokenOrPrefix(ValueError):
+	pass
+class NoConfigError(FileNotFoundError):
+	pass
 def configuration():
     if os.getcwd() != ensure_path.send_path():
         os.chdir(ensure_path.send_path())
-    cfgInDir = False
-    foundPrefix = False
-    for i in os.listdir():
-        if i == "config.config":
-            cfgInDir = True
-            with open('config.config', 'r') as config:
-                for line in config:
-                    splitline = line.split(':')
-                    if splitline[0].lower() == "prefix":
-                        prefix = splitline[1]
-                        foundPrefix = True
-                        break
-                        
-    if not cfgInDir:
-        raise LookupError("Couldn't find config.config")
-    if not foundPrefix:
-        print("Couldn't find a prefix")
-        raise LookupError("Tried to find 'prefix' in config.config, failed to do so")
-    with open('config.config', 'r') as f:
-        foundToken = False
-        for i in f:
-            splitted = i.split(':')
-            if splitted[0].lower() == "token":
-                token = splitted[1]
-                foundToken = True
-                break
-        if foundToken and foundPrefix:
-            return token, prefix
-        else:
-            raise LookupError("Couldn't find the token, can't start the bot LOL")
+    token = ""
+    prefix = ""
+    try:
+    	with open("config.config") as config:
+    		cfg = config.read()
+    except FileNotFoundError as exc:
+    	raise NoConfigError(exc) from exc
+    else:
+    	sep = cfg.split("\n")
+    	configs = {}
+    	try:
+    		configs[sep[0].split(":")[0]] = sep[0].split(":")[1]
+    		configs[sep[1].split(":")[0]] = sep[1].split(":")[1]
+    	except IndexError as exc:
+    		raise MissingTokenOrPrefix("Couldn't find the token and prefix in the config") from exc
+    	else:
+    		token = configs.get("token")
+    		prefix = configs.get("prefix")
+    		if token is None or prefix is None:
+    			raise MissingTokenOrPrefix("Couldn't find the token and prefix in config") from KeyError
+    		else:
+    			return token, prefix
