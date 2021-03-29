62133124ad7394393d0b698f548ba06bda219496
DaRealCodeWritten <sswiftgamer440@gmail.com>
2021-03-28 22:35:30 -0700

    Add new error, error handler, and add some checks

--- util/configuration.py
+++ util/configuration.py
@@ -11,6 +11,10 @@ class NoConfigError(FileNotFoundError):
     pass
 
 
+class InvalidPrefixOrToken(ValueError):
+    pass
+
+
 def configuration() -> Tuple[str, str]:
     if os.getcwd() != ensure_path.send_path():
         os.chdir(ensure_path.send_path())
@@ -25,15 +29,18 @@ def configuration() -> Tuple[str, str]:
         sep = cfg.split("\n")
         configs = {}
         try:
-            configs[sep[0].split(":")[0]] = sep[0].split(":")[1]
-            configs[sep[1].split(":")[0]] = sep[1].split(":")[1]
+            configs[sep[0].split(":")[0].strip(" ")] = sep[0].split(":")[1].strip(" ")
+            configs[sep[1].split(":")[0].strip(" ")] = sep[1].split(":")[1].strip(" ")
         except IndexError as exc:
             raise MissingTokenOrPrefix("Couldn't find the token and prefix in the config") from exc
         else:
             try:
                 token = configs["token"]
                 prefix = configs["prefix"]
+                assert token != "" and prefix != ""
             except KeyError as exc:
                 raise MissingTokenOrPrefix("Couldn't find the token and prefix in config") from exc
+            except AssertionError as exc:
+                raise InvalidPrefixOrToken("The prefix or token was invalid, the token and prefix must not be empty/whitespace") from exc
             else:
                 return token, prefix
