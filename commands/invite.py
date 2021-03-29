aaff13d8a5c6fa3a803624047c66f0c8dfc2b07f
DaRealCodeWritten <sswiftgamer440@gmail.com>
2021-03-29 02:22:26 -0700

    Edit the invite embed some

--- commands/invite.py
+++ commands/invite.py
@@ -6,11 +6,14 @@ class InviteCog(commands.Cog):
     def __init__(self, bot: commands.AutoShardedBot):
         self.bot = bot
 
-    @commands.command(name="invite")
+    @commands.command(name="invite", aliases=["server", "link", "website"])
     async def _invite(self, ctx: commands.Context):
-        embed = discord.Embed(title="Invite link",
-                              description="You can add me using [this link](https://synergile.xyz/invite)",
+        embed = discord.Embed(title="Synergile Invite Link",
+                              description="You can add me to your server using [my invite](https://synergile.xyz/invite)",
                               color=discord.Color.blurple())
+        embed.add_field(name="Come visit us!", value="Visit us in our [support server](https://synergile.xyz/server)", inline=False)
+        embed.add_field(name="Take a peek at our website", value="Find it [here](https://synergile.xyz)")
+        embed.set_thumbnail(url="https://synergile.xyz/images/synergile_logo.png")
         await ctx.send(embed=embed)
 
 
