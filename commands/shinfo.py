0c6dc1927661466bd51bc8d13be88c680f0861a0
DaRealCodeWritten <sswiftgamer440@gmail.com>
2021-03-31 00:52:14 -0700

    Remove _s, and fix commands

--- commands/shinfo.py
+++ commands/shinfo.py
@@ -8,13 +8,17 @@ class ShInfo(commands.Cog, name='Sh_info'):
     @commands.command(description="Displays shard info", hidden=True)
     async def shinfo(self, ctx: commands.Context):
         cshard = round(self.bot.latency*1000)
-        shids = ", ".join(self.bot.shard_ids)
+        shids = ", ".join([str(key) for key in self.bot.shards.keys()])
+        if len(self.bot.shards.keys()) >= 10:
+            shids = "Too Many Shards ({})".format(len(self.bot.shards.keys()))
         nlatencies = []
         for latency in self.bot.latencies:
-            nlatencies.append(round(latency*1000))
+            nlatencies.append(str(round(latency[1]*1000)))
         nlatencies = ", ".join(nlatencies)
+        if len(self.bot.latencies) >= 10:
+            nlatencies = shids
         await ctx.author.send(
-            f"Current Shard Latency: {cshard}\nShard IDs: {shids}\nAverage Shards Latency: {nlatencies}")
+            f"Current Shard Latency: {cshard}\nShard IDs or Shard Count: {shids}\nShards Latency: {nlatencies if len(nlatencies) <= 10 else 'Too Many Shards'}")
         await ctx.send(f"Sent shard info to your DMs, {ctx.author.mention}")
 
 
