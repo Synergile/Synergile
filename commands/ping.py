7a4c96d2f92f3c2f742935ed21a01cdb33f50cc9
DaRealCodeWritten <sswiftgamer440@gmail.com>
2021-03-31 01:54:08 -0700

    Make ping colorful and rework shinfo

--- commands/ping.py
+++ commands/ping.py
@@ -12,12 +12,25 @@ class Ping(commands.Cog, name='Ping'):
     @commands.command(description="Displays connection speed info",
                       help='Calculates the latency of the client to the endpoint based on the time it takes for a response message to be sent to the command message. API latency information is provided by the websocket')
     async def ping(self, ctx):
-        try:
-            m = await ctx.send('Ping?')
-            await m.edit(
-                content=f'🏓 Pong! Latency is {int((snowflake_time(m.id) - snowflake_time(ctx.message.id)).microseconds / 1000)}ms. API Latency is {round(self.bot.latency * 1000)}ms')
-        except Exception as e:
-            await ctx.send(repr(e))
+        colors = {
+            299: discord.Color.green(),
+            300: 0xFFFF00,
+            500: discord.Color.orange(),
+            1000: discord.Color.red()
+        }
+        clr = None
+        for ping, color in colors.items():
+            if ping == 299 and round(self.bot.latency*1000) < ping:
+                clr = color
+            elif round(self.bot.latency*1000) > ping:
+                clr = color
+            if clr is not None and round(self.bot.latency*1000) > ping:
+                clr = color
+        m = await ctx.send("Pinging...")
+        embed = discord.Embed(title="Pong! \U0001f3d3",
+                              description=f"Message Latency: {int((snowflake_time(m.id) - snowflake_time(ctx.message.id)).microseconds / 1000)}ms\nAPI Latency: {round(self.bot.latency*1000)}ms",
+                              color=clr)
+        await m.edit(content="", embed=embed)
 
 
 def setup(bot):
--- commands/shinfo.py
+++ commands/shinfo.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 
 
@@ -7,18 +8,30 @@ class ShInfo(commands.Cog, name='Sh_info'):
 
     @commands.command(description="Displays shard info", hidden=True)
     async def shinfo(self, ctx: commands.Context):
-        cshard = round(self.bot.latency*1000)
-        shids = ", ".join([str(key) for key in self.bot.shards.keys()])
-        if len(self.bot.shards.keys()) >= 10:
-            shids = "Too Many Shards ({})".format(len(self.bot.shards.keys()))
-        nlatencies = []
-        for latency in self.bot.latencies:
-            nlatencies.append(str(round(latency[1]*1000)))
-        nlatencies = ", ".join(nlatencies)
-        if len(self.bot.latencies) >= 10:
-            nlatencies = shids
-        await ctx.author.send(
-            f"Current Shard Latency: {cshard}\nShard IDs or Shard Count: {shids}\nShards Latency: {nlatencies if len(nlatencies) <= 10 else 'Too Many Shards'}")
+        colors = {
+            299: discord.Color.green(),
+            300: 0xFFFF00,
+            500: discord.Color.orange(),
+            1000: discord.Color.red()
+        }
+        shard_ids = ", ".join([str(key) for key in self.bot.shards.keys()])
+        shard_count = len(self.bot.shards)
+        this_shard = str(round(self.bot.latency*1000))
+        clr = None
+        for ping, color in colors.items():
+            if ping == 299 and int(this_shard) < ping:
+                clr = color
+            elif int(this_shard) > ping:
+                clr = color
+            if clr is not None and int(this_shard) > ping:
+                clr = color
+        all_shards = ", ".join([str(round(latency[1]*1000))+"ms" for latency in self.bot.latencies])
+        if shard_count >= 10:
+            all_shards = "Too Many Shards ({})".format(shard_count)
+            shard_ids = "Too Many Shards ({})".format(shard_count)
+        form = f"Shard IDs: ({shard_ids})\nShard Count: {shard_count}\nThis Shard's Latency: {this_shard}ms\nAll Shard Latencies: ({all_shards})"
+        embed = discord.Embed(title="Shard Info", description=form, color=clr)
+        await ctx.author.send(embed=embed)
         await ctx.send(f"Sent shard info to your DMs, {ctx.author.mention}")
 
 
