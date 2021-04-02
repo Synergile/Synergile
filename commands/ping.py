0b6ff204732e71117521953d5a34a75fb1b1cd30
DaRealCodeWritten <SSwiftGamer440@gmail.com>
2021-04-02 02:08:55 -0700

    Change ping to use informative embed colors (#94)
    Add handling for typehint discrepancies in error handler
    Remove underscores from command and file names
    Modify deploy dependency update command to only use lockfile (original intention)
    Ping color design will eventually use a smooth gradient (planned)
    
    * Make some more changes on a whim
    
    * Remove _s, and fix commands
    
    * Make ping colorful and rework shinfo
    
    * Change dependency update command
    
    Piggybacking deploy change into this PR
    
    * Fix shadowing in ping
    
    * Change message latency calculation to save to varaible
    
    No need to redo the same work
    
    * Remove unnecessary str/int casting
    
    Co-authored-by: Walker Gray <55608093+wakfi@users.noreply.github.com>

--- commands/ping.py
+++ commands/ping.py
@@ -12,12 +12,26 @@ class Ping(commands.Cog, name='Ping'):
     @commands.command(description="Displays connection speed info",
                       help='Calculates the latency of the client to the endpoint based on the time it takes for a response message to be sent to the command message. API latency information is provided by the websocket')
     async def ping(self, ctx):
-        try:
-            m = await ctx.send('Ping?')
-            await m.edit(
-                content=f'🏓 Pong! Latency is {int((snowflake_time(m.id) - snowflake_time(ctx.message.id)).microseconds / 1000)}ms. API Latency is {round(self.bot.latency * 1000)}ms')
-        except Exception as e:
-            await ctx.send(repr(e))
+        colors = {
+            299: discord.Color.green(),
+            300: 0xFFFF00,
+            500: discord.Color.orange(),
+            1000: discord.Color.red()
+        }
+        m = await ctx.send("Pinging...")
+        clr = None
+        latency = int((snowflake_time(m.id) - snowflake_time(ctx.message.id)).microseconds / 1000)
+        for png, color in colors.items():
+            if png == 299 and latency < png:
+                clr = color
+            elif latency > png:
+                clr = color
+            if clr is not None and latency > png:
+                clr = color
+        embed = discord.Embed(title="Pong! \U0001f3d3",
+                              description=f"Message Latency: {latency}ms\nAPI Latency: {round(self.bot.latency*1000)}ms",
+                              color=clr)
+        await m.edit(content="", embed=embed)
 
 
 def setup(bot):
---
+++ commands/shinfo.py
@@ -0,0 +1,39 @@
+import discord
+from discord.ext import commands
+
+
+class ShInfo(commands.Cog, name='Sh_info'):
+    def __init__(self, bot: commands.AutoShardedBot):
+        self.bot = bot
+
+    @commands.command(description="Displays shard info", hidden=True)
+    async def shinfo(self, ctx: commands.Context):
+        colors = {
+            299: discord.Color.green(),
+            300: 0xFFFF00,
+            500: discord.Color.orange(),
+            1000: discord.Color.red()
+        }
+        shard_ids = ", ".join([str(key) for key in self.bot.shards.keys()])
+        shard_count = len(self.bot.shards)
+        this_shard = round(self.bot.latency*1000)
+        clr = None
+        for ping, color in colors.items():
+            if ping == 299 and this_shard < ping:
+                clr = color
+            elif this_shard > ping:
+                clr = color
+            if clr is not None and this_shard > ping:
+                clr = color
+        all_shards = ", ".join([str(round(latency[1]*1000))+"ms" for latency in self.bot.latencies])
+        if shard_count >= 10:
+            all_shards = "Too Many Shards ({})".format(shard_count)
+            shard_ids = "Too Many Shards ({})".format(shard_count)
+        form = f"Shard IDs: ({shard_ids})\nShard Count: {shard_count}\nThis Shard's Latency: {this_shard}ms\nAll Shard Latencies: ({all_shards})"
+        embed = discord.Embed(title="Shard Info", description=form, color=clr)
+        await ctx.author.send(embed=embed)
+        await ctx.send(f"Sent shard info to your DMs, {ctx.author.mention}")
+
+
+def setup(bot):
+    bot.add_cog(ShInfo(bot))
